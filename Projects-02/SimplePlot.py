f2fd460a20ea431d9d5b9b2e7886222f4a725709
Srijan Kumar <142709398+Srijan-XI@users.noreply.github.com>
2025-04-26 00:17:07 +0530

    Add files via upload

---
+++ Projects-02/SimplePlot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt  # Import Matplotlib for plotting
+
+fig, ax = plt.subplots()  # Create a figure and a single set of axes
+
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot data points (x-values vs. y-values)
+
+plt.show()  # Display the figure
